81e315bb26e71d69136045047c4a9ddabd11fd1d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-07-01 11:13:36 +0200

    Rewrite Cycle to common Data::Const class

--- Cycle.pm
+++ Cycle.pm
@@ -5,10 +5,10 @@ use strict;
 use warnings;
 
 use Class::Utils qw(set_params);
-use Data::Planning::Cycle;
-use Data::Planning::Cycle::Item;
+use Data::Const::Planning::Cycle::Season;
 use DateTime;
 use Mo::utils 0.06 qw(check_bool);
+use Mo::utils::Hash qw(check_hash);
 
 our $VERSION = 0.02;
 
@@ -22,11 +22,17 @@ sub new {
 	# Mode to present ids.
 	$self->{'mode_id'} = 1;
 
+	# Output structure.
+	$self->{'output_struct'} = {};
+
 	# Process parameters.
 	set_params($self, @params);
 
 	# Check 'mode_id'.
 	check_bool($self, 'mode_id');
+
+	# Check 'output_struct'.
+	check_hash($self, 'output_struct');
 
 	return $self;
 }
@@ -34,73 +40,11 @@ sub new {
 sub data {
 	my $self = shift;
 
-#	my ($act_day, $act_month, $act_year) = (localtime())[3 .. 5];
-#	my $act_month += 1;
-#	my $act_year += 1900;
-#	my $dt_act = DateTime->now;
-#	my $dt_spring = DateTime->new(
-#		'year' => $act_year,
-#		'month' => 3,
-#		'day' => 21,
-#	);
-#	my ($state_spring, $state_summer, $state_autumn, $state_winter) = ('new' x 4);
-#	if (DateTime->compare($dt_act, $dt_spring) == -1) {
-#	}
-	my $spring = Data::Planning::Cycle::Item->new(
-		'description' => 'Spring, also known as springtime, is one of the four temperate seasons, succeeding winter and preceding summer.',
-		# TODO Something relalistic.
-		'dt_changed' => DateTime->now,
-		'dt_created' => DateTime->now,
-		$self->{'mode_id'} ? ('id' => 1) : (),
-		'name' => 'Spring',
-		# TODO Something relalistic.
-		'state' => 'new',
-	);
-	my $summer = Data::Planning::Cycle::Item->new(
-		'description' => 'Summer or summertime is the hottest and brightest of the four temperate seasons, occurring after spring and before autumn.',
-		# TODO Something relalistic.
-		'dt_changed' => DateTime->now,
-		'dt_created' => DateTime->now,
-		$self->{'mode_id'} ? ('id' => 2) : (),
-		'name' => 'Summer',
-		# TODO Something relalistic.
-		'state' => 'new',
-	);
-	my $autumn = Data::Planning::Cycle::Item->new(
-		'description' => 'Autumn, also known as fall, is one of the four temperate seasons on Earth.',
-		# TODO Something relalistic.
-		'dt_changed' => DateTime->now,
-		'dt_created' => DateTime->now,
-		$self->{'mode_id'} ? ('id' => 3) : (),
-		'name' => 'Autumn',
-		# TODO Something relalistic.
-		'state' => 'new',
-	);
-	my $winter = Data::Planning::Cycle::Item->new(
-		'description' => 'Winter is the coldest and darkest season of the year in temperate and polar climates. It occurs after autumn and before spring.',
-		# TODO Something relalistic.
-		'dt_changed' => DateTime->now,
-		'dt_created' => DateTime->now,
-		$self->{'mode_id'} ? ('id' => 4) : (),
-		'name' => 'Winter',
-		# TODO Something relalistic.
-		'state' => 'new',
-	);
+	Data::Const::Planning::Cycle::Season->new(
+		'output_struct' => $self->{'output_struct'},
+	)->data;	
 
-	my $season = Data::Planning::Cycle->new(
-		'dt_created' => DateTime->now,
-		'description' => "A season is a division of the year based on changes in weather, ecology, and the number of daylight hours in a given region. On Earth, seasons are the result of the axial parallelism of Earth's tilted orbit around the Sun",
-		$self->{'mode_id'} ? ('id' => 1) : (),
-		'items' => [$spring, $summer, $autumn, $winter],
-		'name' => 'Season',
-	);
-
-	my $ret_hr = {
-		'cycles' => [$season],
-		'items' => [$spring, $summer, $autumn, $winter],
-	};
-
-	return $ret_hr;
+	return $self->{'output_struct'};
 }
 
 1;
